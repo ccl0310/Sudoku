e9dae86eb72391e156a52799b9d513139f3015cb
ccl0310 <92853590+ccl0310@users.noreply.github.com>
2022-06-13 13:47:06 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include <bits/stdc++.h>
 #define sz 9
 using namespace std;
-
 int arr[sz][sz] = {0};
 int n;
-vector<int> nums = {1,2,3,4,5,6,7,8,9};
 vector<int> row;
 vector<int> col;
 vector<int> block;
@@ -12,6 +10,7 @@ vector<vector<int> > ava_nums;
 vector<int> avability_check(int, int);
 int block_index(int,int);
 int get_empty_num(int,int);
+void print_matrix();
 
 vector<int> avability_check(int row, int col){
 	vector<int> ok;
@@ -45,8 +44,9 @@ vector<int> avability_check(int row, int col){
 	}
 	//block
 	int area = block_index(row,col);
-	for(int i=(area/3)*3;i<(area/3)*3+2;i++){
-		for (int j=(area%3)*3;j<(area%3)*3+2;j++){
+//	cout << area << endl;
+	for(int i=(area/3)*3;i<=(area/3)*3+2;i++){
+		for (int j=(area%3)*3;j<=(area%3)*3+2;j++){
 			if(arr[i][j]!=0){
 				if (blockline[arr[i][j]]==true){
 					return ok;
@@ -83,13 +83,38 @@ int get_empty_num(vector<int> &row, vector<int> &col){
 }
 
 int block_index(int row,int col){
-	row = row / n;
-	col = col / n;
-	int num = row*n + col;
+	row = row / 3;
+	col = col / 3;
+	int num = row*3 + col;
 	return num;
 }
 
-void DFS(int x, int y){
+void DFS(int current,int blank,vector<int> &row,vector<int> &col){
+	if (current == blank){
+		print_matrix();
+		return; 
+	}
+	int r = row[current];
+	int c = col[current];
+	vector<int> candidates = avability_check(r,c);
+	if (candidates.empty()) return;
+	for(int i=0;i<candidates.size();i++){
+		int value = candidates[i];
+		arr[r][c] = value;
+		DFS(current+1,blank,row,col);
+		arr[r][c] = 0;
+	}
+}
+
+void print_matrix(){
+	for(int i=0;i<3*n;i++){
+		for(int j=0;j<3*n;j++){
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	return;
 }
 
 int main(){
@@ -102,19 +127,20 @@ int main(){
 	}
 	
 	int blank = get_empty_num(row,col);
-	for (int i=0;i<blank;i++){
-		vector<int> fl = avability_check(row[i],col[i]);
-		if (!fl.empty()){
-			cout << row[i] << " " << col[i] << " ";
-			cout << "(";
-			for(int j=0;j<fl.size();j++){
-				cout << fl[j] << " ";
-			}
-			cout << ")" << endl;
-		}
-		else {
-			cout << "NO SOLUTION" << endl;
-			break;
-		}
-	}
+	DFS(0,blank,row,col);
+//	for (int i=0;i<blank;i++){
+//		vector<int> fl = avability_check(row[i],col[i]);
+//		if (!fl.empty()){
+//			cout << row[i] << " " << col[i] << " ";
+//			cout << "(";
+//			for(int j=0;j<fl.size();j++){
+//				cout << fl[j] << " ";
+//			}
+//			cout << ")" << endl;
+//		}
+//		else {
+//			cout << "NO SOLUTION" << endl;
+//			break;
+//		}
+//	}
 }
